54b0298337572e8384738e7dc3cfe85da2c7b211
Carlos Zegarra <cgzegarra@gmail.com>
2020-02-08 02:20:46 -0500

    course work completed

--- src/main/java/com/busyqa/coursework/wk1_answer/CheckOddEven.java
+++ src/main/java/com/busyqa/coursework/wk1_answer/CheckOddEven.java
@@ -3,7 +3,7 @@ package com.busyqa.coursework.wk1_answer;
 public class CheckOddEven {
     public static void main(String[] args) {  // Program entry point
         int number = 50;       // Set the value of "number" here!
-//        System.out.println("The number is " + number);
+         System.out.println("The number is " + number);
 //        if ( ...... ) {
 //            System.out.println( ...... );
 //        } else {
