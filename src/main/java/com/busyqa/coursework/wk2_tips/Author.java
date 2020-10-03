5d229ad8813d1d31d68de971ffdcd4469a5fb4af
Carlos Zegarra <cgzegarra@gmail.com>
2020-10-03 01:41:23 -0400

    examples Book and Author added

--- src/main/java/com/busyqa/coursework/wk2_tips/Author.java
+++ src/main/java/com/busyqa/coursework/wk2_tips/Author.java
@@ -1,4 +1,37 @@
 package com.busyqa.coursework.wk2_tips;
 
 public class Author {
+
+    /* Instance Variables */
+
+    private String firstName = "";
+    private String lastName = "";
+
+    /* Constructor */
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    /* Methods */
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String toString() {
+        return firstName + " " + lastName;
+    }
 }
--- src/main/java/com/busyqa/coursework/wk2_tips/Book.java
+++ src/main/java/com/busyqa/coursework/wk2_tips/Book.java
@@ -1,4 +1,49 @@
 package com.busyqa.coursework.wk2_tips;
 
 public class Book {
+
+
+    /* Instance Variables */
+
+    private String title;
+    private Author author;
+    private double price;
+
+    /* Constructor */
+    public Book(String title, String authorFirstName, String authorLastName, double price) {
+        this.author = new Author(authorFirstName, authorLastName);
+        this.title = title;
+        this.price = price;
+    }
+
+    /* Methods */
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String toString() {
+        return this.title + " by " + this.author.getFirstName() + " " + this.author.getLastName() + " for $" + this.price;
+    }
+
 }
